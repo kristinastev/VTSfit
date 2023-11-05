5872970cdab89c80f7095f1c373bad22aa4b4f77
kristinastev <149689425+kristinastev@users.noreply.github.com>
2023-11-05 13:58:01 +0100

    1.0

---
+++ train.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n-pose.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+model.train(data="config.yaml", epochs=1)  # train the model
